cd091ce07e3bd32acc8e84121d19384ef313f7f0
Biniyam Wahima <biniyamwahima@gmail.com>
2023-11-09 01:54:49 +0530

    'Final'

--- ProductPal/settings.py
+++ ProductPal/settings.py
@@ -79,23 +79,23 @@ WSGI_APPLICATION = 'ProductPal.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'productpal$default',
-        'USER': 'productpal',
-        'PASSWORD': '',
-        'HOST': 'productpal.mysql.pythonanywhere-services.com',
-        'PORT': '3306'
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'productpal$default',
+#         'USER': 'productpal',
+#         'PASSWORD': '',
+#         'HOST': 'productpal.mysql.pythonanywhere-services.com',
+#         'PORT': '3306'
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 
 # Password validation
